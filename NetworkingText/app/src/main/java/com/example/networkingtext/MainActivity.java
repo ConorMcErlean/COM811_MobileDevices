ecf6dffd6e85e1370e71c1a75c988f6af428ac06
Conor Mc <Conor@cmcerlean.co.uk>
2020-04-02 15:45:13 +0100

    Adding OpenHttpConnection() method

--- NetworkingText/app/src/main/java/com/example/networkingtext/MainActivity.java
+++ NetworkingText/app/src/main/java/com/example/networkingtext/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.networkingtext;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Open Connection To Internet
+    private InputStream OpenHttpConnection(String urlString) throws IOException{
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+
+        if(!(conn instanceof HttpURLConnection)) throw new IOException(
+                "Not a HTTP connection");
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if(response == HttpURLConnection.HTTP_OK){
+                in =httpConn.getInputStream();
+            }
+        }
+        catch (Exception ex){
+            Log.d("Networking", ex.getLocalizedMessage());
+            throw new IOException("Error connecting...");
+        }
+        return in;
     }
 }
